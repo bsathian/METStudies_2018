bde15210a6fdff42434d04173fe02cb7a5734814
Samuel May <sammay@ucla.edu>
2018-10-16 16:29:43 -0700

    Add CMS4-making scripts

---
+++ BabyMaker/ducks.py
@@ -0,0 +1,93 @@
+import time
+import itertools
+import json
+
+from metis.Sample import DBSSample
+from metis.CMSSWTask import CMSSWTask
+from metis.StatsParser import StatsParser
+
+import os, sys
+
+tar_name = "lib_CMS4Plus_V10-02-01.tar.gz"
+os.system("mtarfile -b %s --xz" % tar_name) 
+
+if __name__ == "__main__":
+
+    pds = ["DoubleMuon", "EGamma"]
+    proc_vers = [
+	#("Run2018A", "v1"),
+	#("Run2018A", "v2"),
+	#("Run2018A", "v3"),
+	#("Run2018B", "v1"),
+	#("Run2018B", "v2"),
+	#("Run2018C", "v1"),
+        #("Run2018C", "v2"),
+	("Run2018C", "v3"),
+	#("Run2018D", "v1"),
+        ("Run2018D", "v2"),
+	]	
+
+    dataset_names = ["/{0}/{1}-PromptReco-{2}/MINIAOD".format(x[0],x[1][0],x[1][1]) for x in itertools.product(pds,proc_vers)]
+
+    mc_samples = ["/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall18MiniAOD-102X_upgrade2018_realistic_v12_ext1-v1/MINIAODSIM"]
+
+    cmsswver = "CMSSW_10_2_5"
+    tarfile = tar_name
+
+    for i in range(10000):
+
+        total_summary = {}
+        total_counts = {}
+	for dsname in mc_samples:
+            open_dataset = False
+            task = CMSSWTask(
+                    sample = DBSSample(dataset=dsname),
+                    open_dataset = open_dataset,
+                    flush = ((i+1)%48==0),
+                    # flush = ((i)%48==0), 
+                    events_per_output = 100e3,
+                    output_name = "merged_ntuple.root",
+                    tag = "CMS4_V10-02-01_allPfCands",
+                    #tag = "CMS4_V00-00-06_allPfCands",
+                    global_tag = "", # if global tag blank, one from DBS is used
+
+                    #pset = "/home/users/sjmay/Commissioning2017/Commissioning2017_MET/BabyMaker/CMSSW_10_1_0/src/CMS3/NtupleMaker/test/main_pset.py",
+                    pset = cmsswver + "/src/CMS3/NtupleMaker/test/main_pset.py",
+                    pset_args = "data=False prompt=False",
+                    cmssw_version = cmsswver,
+                    publish_to_dis = False,
+                    tarfile = tarfile,
+                    is_data = True,
+            )
+            task.process()
+            total_summary[dsname] = task.get_task_summary()
+
+        for dsname in dataset_names:
+            open_dataset = False
+            task = CMSSWTask(
+                    sample = DBSSample(dataset=dsname),
+                    open_dataset = open_dataset,
+                    flush = ((i+1)%48==0), 
+                    # flush = ((i)%48==0), 
+                    events_per_output = 100e3,
+                    output_name = "merged_ntuple.root",
+		    tag = "CMS4_V10-02-01_allPfCands",
+                    #tag = "CMS4_V00-00-06_allPfCands",
+                    global_tag = "", # if global tag blank, one from DBS is used
+                    
+		    #pset = "/home/users/sjmay/Commissioning2017/Commissioning2017_MET/BabyMaker/CMSSW_10_1_0/src/CMS3/NtupleMaker/test/main_pset.py",
+		    pset = cmsswver + "/src/CMS3/NtupleMaker/test/main_pset.py",
+                    pset_args = "data=True prompt=True",
+                    cmssw_version = cmsswver,
+		    publish_to_dis = False,
+                    tarfile = tarfile,
+                    is_data = True,
+            )
+            task.process()
+            total_summary[dsname] = task.get_task_summary()
+
+        StatsParser(data=total_summary, webdir="~/public_html/dump/metis/").do()
+
+        # time.sleep(1.*3600)
+        time.sleep(10.*60)
+
---
+++ BabyMaker/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#USER INPUTS
+CMS3Tag=102X
+CMSSW_release=CMSSW_10_2_5
+CMSSW_release_name=    #Leave this blank if you don't know what it is.  It's just a marker in case you have multiple identical directories.  Don't forget the underscore!
+export SCRAM_ARCH=slc6_amd64_gcc700
+
+#--Here there be dragons----
+export CMS_PATH=/cvmfs/cms.cern.ch
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+scramv1 p -n ${CMSSW_release}${CMSSW_release_name} CMSSW $CMSSW_release
+cd ${CMSSW_release}${CMSSW_release_name}
+eval `scramv1 runtime -sh`
+
+# # SLOW -- default
+# git cms-init
+
+# FAST -- because there's a million tags to fetch nowadays
+curl -O "https://raw.githubusercontent.com/cms-sw/cms-git-tools/master/git-cms-init"
+chmod u+x git-cms-init
+sed -i 's/--tags//' git-cms-init
+sed -i 's/git fetch/git fetch --no-tags/' git-cms-init
+./git-cms-init
+
+cd src
+
+git clone git@github.com:sam-may/NtupleMaker.git CMS3/NtupleMaker
+cd CMS3/NtupleMaker
+git checkout $CMS3Tag
+source setup/patchesToSource.sh
+cd $CMSSW_BASE/src
+scram b -j 25
+cd ..
+
+# see comment in patchesToSource.sh
+rm $CMSSW_BASE/lib/$SCRAM_ARCH/.poisonededmplugincache
+
+cd $CMSSW_BASE/src/CMS3/NtupleMaker/test/
