7817ad0ed282a0647b788791ee4da963e17e62fa
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2020-02-25 13:12:20 +0100

    typo fixing round 2

--- Looper/ScanChain.C
+++ Looper/ScanChain.C
@@ -323,7 +323,7 @@ int ScanChain(TChain* chain, TString output_name, vector<TString> vWeightFile, b
       bool isElEvt;
       int id1(-1), id2(-1);
       if (cms3.evt_isRealData()) {
-        if (!(passHLTTriggerPattern(elT1) || passHLTTriggerPattern(elT2)  passHLTTriggerPattern(muT1) || passHLTTriggerPattern(muT2))) continue;
+        if (!(passHLTTriggerPattern(elT1) || passHLTTriggerPattern(elT2) ||  passHLTTriggerPattern(muT1) || passHLTTriggerPattern(muT2))) continue;
         if (passHLTTriggerPattern(elT1) || passHLTTriggerPattern(elT2))                                          isElEvt = true;
         else if (passHLTTriggerPattern(muT1) || passHLTTriggerPattern(muT2))      isElEvt = false;
       }
@@ -331,8 +331,11 @@ int ScanChain(TChain* chain, TString output_name, vector<TString> vWeightFile, b
       else {
         if (lepsPassPOG(true, id1, id2))
           isElEvt = true;
+
         else if (lepsPassPOG(false, id1, id2))
-          isElE)
+          isElEvt = false;
+        else continue;
+      }
 
       if (selection == 3) {
 	if (!isElEvt)				continue;
@@ -390,7 +393,7 @@ int ScanChain(TChain* chain, TString output_name, vector<TString> vWeightFile, b
             lepsf_weights.push_back( h_eleweights_id  ->GetBinContent( h_eleweights_id  ->FindBin( abs(lepeta_trailing), min_leppt_trailing)));
 
             lepsf_weights.push_back( h_eleweightsiso  ->GetBinContent( h_eleweightsiso  ->FindBin( abs(lepeta_leading), min_leppt_leading )) );
-            lepsf_weights.push_back( h_eleweightsiso  ->GetBinContent( h_eleweightsiso  ->FindBin( abs_lepeta_trailing, min_leppt_trailing )) );
+            lepsf_weights.push_back( h_eleweightsiso  ->GetBinContent( h_eleweightsiso  ->FindBin( abs(lepeta_trailing), min_leppt_trailing )) );
 
             lepsf_weights.push_back( h_eleweights_conv  ->GetBinContent( h_eleweights_conv  ->FindBin( abs(lepeta_leading), min_leppt_leading )) );
             lepsf_weights.push_back( h_eleweights_conv  ->GetBinContent( h_eleweights_conv  ->FindBin( abs(lepeta_trailing), min_leppt_trailing )) );
